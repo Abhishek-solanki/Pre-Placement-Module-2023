dd0493d7c4b9bb6603b07bd9def04b67bfe4b474
Abhishek-solanki <abhishek.1923me1060@kiet.edu>
2022-08-02 12:28:27 +0530

    Update two sum - insert Array insert.cpp

--- ALGO II/Day 2/two sum - insert Array insert.cpp
+++ ALGO II/Day 2/two sum - insert Array insert.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int i=0, j=numbers.size()-1;
-        vector<int> v;
-        while(i<j){
-            if(numbers[i] + numbers[j]  == target){
-                v.push_back(i+1);
-                v.push_back(j+1);
-                break;
-            }
-            else if(numbers[i] + numbers[j]  < target ){
-                i++;
-            }
-            else{
-                j--;
-            }
-        }
-        return v;
+    void reverseString(vector<char>& s) {
+        for(int i=0;i<s.size()/2;i++)
+            swap(s[i],s[s.size()-i-1]);
     }
 };
